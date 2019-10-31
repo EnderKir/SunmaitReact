5bab34d5c1cd7fdcffb308912a182c9c8bf59d1b
Alex <al.vedilo08@mail.ru>
2019-10-31 18:50:16 +0300

    add activeClass to mobileMenu

--- src/components/mobileMenu/MobileMenu.js
+++ src/components/mobileMenu/MobileMenu.js
@@ -1,16 +1,37 @@
 import React from "react";
-import { MobileNavLink } from "../mobileNavLink/MobileNavLink";
+import { NavLink } from "react-router-dom";
 import { MobileNavSearch } from "../mobileNavSearch/MobileNavSearch";
-import "./mobileMenu.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import "./mobileMenu.css";
 export function MobileMenu() {
   return (
     <div className="mobile-navigation" id="mobile-navigation">
       <MobileNavSearch />
-      <MobileNavLink inner="Home" path="/home"/>
-      <MobileNavLink inner="Projects" path='/projects'/>
-      <MobileNavLink inner="Guides" path='/guides'/>
-      <MobileNavLink inner="Blog" path='/blog'/>
-      <MobileNavLink inner="Training &amp; Certification" path="/trainingAndCertifation"/>
+      <NavLink to="/home" className="navbar-link" activeClassName="active">
+        Home
+        <FontAwesomeIcon icon={faChevronRight} size="lg" color="" />
+      </NavLink>
+      <NavLink to="/projects" className="navbar-link" activeClassName="active">
+        Projects
+        <FontAwesomeIcon icon={faChevronRight} size="lg" color="" />
+      </NavLink>
+      <NavLink to="/guides" className="navbar-link" activeClassName="active">
+        Guides
+        <FontAwesomeIcon icon={faChevronRight} size="lg" color="" />
+      </NavLink>
+      <NavLink to="/blog" className="navbar-link" activeClassName="active">
+        Blog
+        <FontAwesomeIcon icon={faChevronRight} size="lg" color="" />
+      </NavLink>
+      <NavLink
+        to="/trainingAndCertifation"
+        className="navbar-link"
+        activeClassName="active"
+      >
+        Training &amp; Certification
+        <FontAwesomeIcon icon={faChevronRight} size="lg" color="" />
+      </NavLink>
     </div>
   );
 }
