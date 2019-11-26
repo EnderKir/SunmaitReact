de317272b6ddf110bcd97535f5c1ecdbe8370b9e
Alex <al.vedilo08@mail.ru>
2019-11-26 21:26:37 +0300

    add h2 for no match search

--- src/components/pages/projectPage/ProjectPage.js
+++ src/components/pages/projectPage/ProjectPage.js
@@ -6,9 +6,7 @@ import "./projectPage.css";
 
 export const ProjectPage = ({ projectsArray }) => {
   return (
-    <div
-      className="body-container"
-    >
+    <div className="body-container">
       <main className="main-body">
         <h1>Main Projects</h1>
         <p className="about">
@@ -17,6 +15,7 @@ export const ProjectPage = ({ projectsArray }) => {
           <strong>Spring Project</strong> to help you build it. Start small and
           use just what you need – <strong>Spring is modular by design</strong>.
         </p>
+        {(projectsArray.length === 0 && <h2>No Matches Found</h2>)}
         <ProjectsContainer projectsArray={projectsArray} />
       </main>
     </div>
--- src/components/projectList/ProjectsList.js
+++ src/components/projectList/ProjectsList.js
@@ -6,5 +6,10 @@ export const ProjectsList = ({ projectsArray }) => {
   const projectList = projectsArray.map((project, i) => {
     return <ProjectItem key={i} project={project} />;
   });
+  // if (projectList.length > 0) {
+  //   return projectList;
+  // } else {
+  //   return <h2 style={{ justifyItems: "center" }}>No Mathes</h2>;
+  // }
   return projectList;
 };
