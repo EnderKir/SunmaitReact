efe15a4070740b390274615bfce9eb6bbbaa1622
Alex <al.vedilo08@mail.ru>
2019-11-26 20:21:28 +0300

    close mobileMenu by click on menu link

--- src/components/mobileMenu/MobileMenu.js
+++ src/components/mobileMenu/MobileMenu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 import { MobileNavSearch } from "../mobileNavSearch/MobileNavSearch";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,7 +8,8 @@ import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 import "./mobileMenu.css";
 
-export const MobileMenu = ({mobileFlag}) => {
+export const MobileMenu = ({ mobileFlag }) => {
+  const dispatch = useDispatch();
   return (
     <div
       className="mobile-navigation"
@@ -15,19 +17,39 @@ export const MobileMenu = ({mobileFlag}) => {
       style={{ left: mobileFlag ? "0" : "-400px" }}
     >
       <MobileNavSearch />
-      <NavLink to="/" className="navbar-link" activeClassName="active">
+      <NavLink
+        to="/"
+        className="navbar-link"
+        activeClassName="active"
+        onClick={() => dispatch({ type: "CLOSE_MOBILE_MENU" })}
+      >
         Home
         <FontAwesomeIcon icon={faChevronRight} size="lg" color="" />
       </NavLink>
-      <NavLink to="/projects" className="navbar-link" activeClassName="active">
+      <NavLink
+        to="/projects"
+        className="navbar-link"
+        activeClassName="active"
+        onClick={() => dispatch({ type: "CLOSE_MOBILE_MENU" })}
+      >
         Projects
         <FontAwesomeIcon icon={faChevronRight} size="lg" color="" />
       </NavLink>
-      <NavLink to="/guides" className="navbar-link" activeClassName="active">
+      <NavLink
+        to="/guides"
+        className="navbar-link"
+        activeClassName="active"
+        onClick={() => dispatch({ type: "CLOSE_MOBILE_MENU" })}
+      >
         Guides
         <FontAwesomeIcon icon={faChevronRight} size="lg" color="" />
       </NavLink>
-      <NavLink to="/blog" className="navbar-link" activeClassName="active">
+      <NavLink
+        to="/blog"
+        className="navbar-link"
+        activeClassName="active"
+        onClick={() => dispatch({ type: "CLOSE_MOBILE_MENU" })}
+      >
         Blog
         <FontAwesomeIcon icon={faChevronRight} size="lg" color="" />
       </NavLink>
@@ -35,6 +57,7 @@ export const MobileMenu = ({mobileFlag}) => {
         to="/trainingAndCertifation"
         className="navbar-link"
         activeClassName="active"
+        onClick={() => dispatch({ type: "CLOSE_MOBILE_MENU" })}
       >
         Training &amp; Certification
         <FontAwesomeIcon icon={faChevronRight} size="lg" color="" />
@@ -42,4 +65,3 @@ export const MobileMenu = ({mobileFlag}) => {
     </div>
   );
 };
-
